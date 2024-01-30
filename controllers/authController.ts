c163f502dbe5e3b5a6a76670678b82fe45dbbc0c
Walter Fritsche <wfritsche95@protonmail.com>
2024-01-30 10:59:34 -0600

    added signup and some of the login logic

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,12 +1,61 @@
 import { type Request, type Response, type NextFunction } from "express";
+import { body, validationResult, matchedData } from "express-validator";
+import User from "../models/User";
+import bcrypt from "bcryptjs";
+import asyncHandler from "express-async-handler";
 
-const signup_POST = (req: Request, res: Response) => {
-  res.send("POST -> /signup is not yet implemented");
-};
+const signup_POST = [
+  body("name").isLength({ min: 1, max: 24 }).escape(),
+  body("email").isEmail().escape(),
+  body("password").isLength({ min: 6 }),
+  asyncHandler(async (req: Request, res: Response) => {
+    const result = validationResult(req);
 
-const login_POST = (req: Request, res: Response) => {
-  res.send("POST -> /signup is not yet implemented");
-};
+    if (result.isEmpty()) {
+      const { name, email, password } = matchedData(req);
+      // console.log(email, password);
+      const hash = await bcrypt.hash(password, 8);
+      const user = new User({
+        name,
+        email,
+        password: hash,
+        isAuthor: false,
+        isAdmin: false,
+      });
+      await user.save();
+
+      res.json({ success: true, userId: user._id });
+    } else {
+      // validation result failed
+      console.log("validation result failed");
+      const { email, password } = matchedData(req);
+      console.log(email, password);
+      res.status(400).json({
+        errors: result.array(),
+      });
+    }
+  }),
+];
+
+const login_POST = [
+  body("email").isEmail().escape(),
+  body("password").isLength({ min: 6 }),
+  asyncHandler(async (req: Request, res: Response) => {
+    const result = validationResult(req);
+
+    if (result.isEmpty()) {
+      const { email, password } = matchedData(req);
+      const user = await User.find({ email: email }).exec();
+      if (user) {
+        // @ts-ignore
+        const isValid = await bcrypt.compare(password, user.password);
+        if (isValid) {
+          // req.session.user = { userId: user._id}
+        }
+      }
+    }
+  }),
+];
 
 export default {
   signup_POST,
