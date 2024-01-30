97776fdb072e0b55cd8f391f251bff1f82522da9
Walter Fritsche <wfritsche95@protonmail.com>
2024-01-30 10:57:28 -0600

    setup sessions

--- app.ts
+++ app.ts
@@ -5,6 +5,8 @@ import "dotenv/config";
 import indexRouter from "./routes/index";
 import postRouter from "./routes/postRoutes";
 import mongoose from "mongoose";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -16,7 +18,25 @@ main().catch((err) => console.log(err));
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(cookieParser());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET as string,
+    resave: false,
+    saveUninitialized: true, // normally true in other tutorials but false could work
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB,
+      collectionName: "sessions",
+    }),
+    cookie: {
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+      httpOnly: true,
+      secure: false,
+      path: "/",
+      sameSite: "none",
+    },
+  }),
+);
 
 app.use("/", indexRouter);
 app.use(postRouter);
