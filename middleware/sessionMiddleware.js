19b34bd4774725075d8495b16223f8e0d2e8bc3d
Walter Fritsche <wfritsche95@protonmail.com>
2024-01-31 13:40:59 -0600

    added 2 middleware functions

--- middleware/sessionMiddleware.js
+++ middleware/sessionMiddleware.js
@@ -11,6 +11,34 @@ const getUserFromSession = asyncHandler(async (req, res, next) => {
   }
 });
 
+const isAuthed = (req, res, next) => {
+  if (req.session.user) {
+    return next();
+  } else {
+    const error = new Error();
+    error.message =
+      "You're not authenticated. Pleace login before using this resource";
+    error.status = 401;
+    res.status(401).json({ success: false, errors: error });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  // this middleware relies on the fact that the auth status has
+  // been checked and the user is authenticated.
+  if (req.user.isAdmin) {
+    return next();
+  } else {
+    const error = new Error();
+    error.message =
+      "You don't have the admin privilages needed to use this resource.";
+    error.status = 403;
+    res.status(403).json({ success: false, errors: error });
+  }
+};
+
 module.exports = {
   getUserFromSession,
+  isAuthed,
+  isAdmin,
 };
